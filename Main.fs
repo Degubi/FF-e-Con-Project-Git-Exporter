1399ca421f71bfae826a9d142fa7eca372d6df17
Degubi <gergonoorbi@gmail.com>
2021-02-12 16:36:49 +0100

    Remove hardcoded zip file name

--- Main.fs
+++ Main.fs
@@ -4,51 +4,55 @@ open System.IO
 open System.IO.Compression
 open System.Xml.Linq
 
-let unzipTmpFolder = "tmp_unzip"
-
-let rec exportFilesFromRuleFolder(folder: XElement, modelName: string, sharedCounters: int[]) =
+let rec exportRulesFromFolder(folder: XElement, modelName: string, modelSharedCounters: int[]) =
     let folderName = folder.Attribute(XName.Get "LABEL").Value
     let folderPath = $"{modelName}/{folderName}"
 
-    folder.Elements(XName.Get "RULES") |> Seq.iter(fun k -> exportFilesFromRuleFolder(k, $"{modelName}/{folderName}", sharedCounters))
+    folder.Elements(XName.Get "RULES") |> Seq.iter(fun k -> exportRulesFromFolder(k, $"{modelName}/{folderName}", modelSharedCounters))
     folder.Elements(XName.Get "RULE") |> Seq.iter(fun rule ->
         let ruleName = rule.Attribute(XName.Get "LABEL").Value.Replace('/', '_')
         let condition = rule.Element(XName.Get "CONDITION").Value
         let implication = rule.Element(XName.Get "IMPLICATION").Value
 
-        sharedCounters.[0] <- sharedCounters.[0] + 1
+        modelSharedCounters.[0] <- modelSharedCounters.[0] + 1
         Directory.CreateDirectory(folderPath) |> ignore
-        File.WriteAllText($"{folderPath}/{sharedCounters.[0]}_{ruleName}.vbs", $"/*\nCondition: '{condition}'\n*/\n\n{implication}")
+        File.WriteAllText($"{folderPath}/{modelSharedCounters.[0]}_{ruleName}.vbs", $"/*\nCondition: '{condition}'\n*/\n\n{implication}")
     )
 
-let exportTopLevelRuleFolder(modelFileName: string) =
+let exportModelFile(modelFileEntry: ZipArchiveEntry) =
+    let modelFileName = modelFileEntry.Name
     let modelName = string modelFileName.[0] + modelFileName.Substring(1, modelFileName.IndexOf(',') - 1).ToLower()
-    let sharedCounters = [| 0 |]
+    let modelSharedCounters = [| 0 |]
 
     if Directory.Exists modelName then
         Directory.Delete(modelName, true)
 
     printfn $"Exporting model: {modelName}"
-    XDocument.Load($"{unzipTmpFolder}/Models/{modelFileName}")
+
+    use modelFileStream = modelFileEntry.Open()
+    XDocument.Load(modelFileStream)
              .Element(XName.Get "CLASS")
              .Element(XName.Get "RULES")
-             .Elements(XName.Get "RULES") |> Seq.iter(fun k -> exportFilesFromRuleFolder(k, modelName, sharedCounters))
+             .Elements(XName.Get "RULES") |> Seq.iter(fun k -> exportRulesFromFolder(k, modelName, modelSharedCounters))
 
 
-printfn "Enter commit message!"
-let commitMessage = Console.ReadLine()
+match Directory.GetFiles "." |> Seq.tryFind(fun k -> k.EndsWith ".zip") with
+    | None -> printfn "No input .zip file found in current folder!"
+    | Some inputZipFile ->
+        printfn $"Using zip file: '{inputZipFile |> Path.GetFileName}'\n"
+        printfn "Enter commit message!"
 
-printfn "Using zip file: 'Export.zip'"
-ZipFile.ExtractToDirectory("Export.zip", unzipTmpFolder)
-Directory.GetFiles $"{unzipTmpFolder}/Models" |> Seq.map(Path.GetFileName)
-                                              |> Seq.filter(fun k -> k.EndsWith ".xml" && k.[0] <> 'I')
-                                              |> Seq.iter(exportTopLevelRuleFolder)
-Directory.Delete(unzipTmpFolder, true)
-File.Delete("Export.zip")
+        let commitMessage = Console.ReadLine()
+        use zipFile = ZipFile.OpenRead(inputZipFile)
 
-printfn "Publishing to GitHub"
-Process.Start("cmd.exe", $"/c \"git config --global core.autocrlf true && git add -A 2>nul && git commit -m \"{commitMessage}\" 2>nul && git push origin master 2>nul\"")
-       .WaitForExit()
+        zipFile.Entries |> Seq.filter(fun k -> k.Name.EndsWith(".xml") && k.Name.[0] <> 'I')
+                        |> Seq.iter(exportModelFile)
 
-printfn "Done! Press enter to exit"
+        File.Delete(inputZipFile)
+
+        printfn "Publishing to GitHub"
+        Process.Start("cmd.exe", $"/c \"git config --global core.autocrlf true && git add -A 2>nul && git commit -m \"{commitMessage}\" 2>nul && git push origin master 2>nul\"")
+               .WaitForExit()
+
+printfn "\nDone! Press enter to exit"
 Console.Read() |> ignore
